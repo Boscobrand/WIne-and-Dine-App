299549c2c92ea36d7054645370a5b43efeffb4e2
Tony Bosco <Tony.Bosco@boscobrand.com>
2022-11-22 15:25:30 -0500

    photoclick fix, feed fix

--- my-app/src/Components/ResultList.js
+++ my-app/src/Components/ResultList.js
@@ -5,24 +5,24 @@ import React from 'react';
 
 function ResultList(props) {
      
-     return(
+    return(
 
-         <div className="recipeBox">
-            {props.recipes.map(recipe => {
-                return(
-                    <div className = "recipeCard">
-                        <div className="name">{recipe.recipe.label}</div>
-                        <img src={recipe.recipe.image} alt={recipe.recipe.label} className="pic"/>
-                        <div><a href={recipe.recipe.url} target="_blank" className="B-url" rel="noopener noreferrer"> Recipe </a></div>
-!                        <div><button className="i-button">Ingredients</button></div>
-                    </div>    
-                )
-            
-            })}
-            
-        </div>
+        <div className="recipeBox">
+           {props.recipes.map(recipe => {
+               return(
+                   <div className = "recipeCard">
+                       <div className="name">{recipe.recipe.label}</div>
+                       <a href={recipe.recipe.url} target="_blank" className="B-url" rel="noopener noreferrer"> <img src={recipe.recipe.image} alt={recipe.recipe.label} className="pic"/></a>
+                       {/* <div>Recipe </a></div> */}
+                       {/* <div><button className="i-button">Ingredients</button></div> */}
+                   </div>    
+               )
+           
+           })}
+           
+       </div>
 
-     )
+    )
 
 }
 export default ResultList;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import {BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 //<React.StrictMode><App /></React.StrictMode>
 root.render(<Router><App /></Router>);
 
